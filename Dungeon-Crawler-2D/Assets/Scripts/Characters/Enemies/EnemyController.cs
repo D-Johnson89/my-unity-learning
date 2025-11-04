6079d7464ced1e26bab161e7eecef4dd83688050
D-Johnson89 <d.l.johnson3009@gmail.com>
2025-11-04 13:08:17 -0500

    fix: update enemy sprite names, save enemy prefabs

--- Dungeon-Crawler-2D/Assets/Scripts/Characters/Enemies/EnemyController.cs
+++ Dungeon-Crawler-2D/Assets/Scripts/Characters/Enemies/EnemyController.cs
@@ -1,30 +1,47 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Base Enemy AI/Movement
 public class EnemyController : CharacterBase
 {
 
-    public float detectionRadius = 5f;
-    public float leashDistance = 10f;
-    public float waitTime = 1f;
+    // Patrol variables
+    public GameObject wayPoint1;
+    public GameObject wayPoint2;
     public Transform[] waypoints;
     private int currentWaypointIndex = 0;
+    private Transform currentPoint;
+
+    // Enemy variables
+    private Rigidbody2D rb;
+    public float detectionRadius = 5f;
+    public float leashDistance = 10f;
+    public float waitTime = 1f;  
     private bool isWaiting = false;
     private Vector3 spawnPoint;
-    private Transform player;
-
+    
+    // State variables
     enum State { Patrol, Chase, Return }
     private State currentState = State.Patrol;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        currentPoint = wayPoint1.transfrom;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        switch(currentState)
+        Vector2 point = currentPoint.position - transform.position;
+
+        switch (currentState)
         {
             case State.Patrol:
                 Patrol();
-                CheckForPlayer;
+                CheckForPlayer();
                 break;
             case State.Chase:
                 ChasePlayer();
@@ -35,4 +52,15 @@ public class EnemyController : CharacterBase
                 break;
         }
     }
+
+    void Patrol()
+    {
+        
+    }
+    
+    protected override void Die()
+    {
+        Destroy(gameObject);
+    }
 }
+*/
