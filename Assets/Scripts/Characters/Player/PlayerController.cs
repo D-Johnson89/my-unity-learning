ff9161d8ccc8c2a985da26fed8ce9e4309bb4195
Dustin Johnson <d.l.johnson3009@gmail.com>
2026-03-14 14:14:53 -0400

    fix: refactor patrol movement and logic - change movement to rigidbody from transform - move movement to fixedupdate, sort logic to work independently - fix character movement

--- Assets/Scripts/Characters/Player/PlayerController.cs
+++ Assets/Scripts/Characters/Player/PlayerController.cs
@@ -7,12 +7,18 @@ public class PlayerController : CharacterBase
     [Header("Player Specific")]
     public int maxMana = 50;
     public int currentMana;
+    public float moveSpeed = 3f;
+    public float horizontalInput;
+    public float verticalInput;
 
     void Update()
     {
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
-        Move(h, v);
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+    }
+    void FixedUpdate()
+    {
+        Move(horizontalInput, verticalInput, moveSpeed);
     }
 
     protected override void Die()
